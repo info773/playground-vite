cf60ddaaea6118caa01c2e1372e48339e3f21d4c
Berry <info@felix-behrens.de>
2025-10-27 19:49:44 +0100

    Tailwind CSS V4 Crahs Course 2025 - 45:36.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
-import './App.css'
+import "./App.css";
+
+// YT: 45:36
 
 function App() {
-  return <h1 className='text-9xl'>Hello World</h1>
-  }
+  return (
+    <>
+      <div className="bg-teal-100 min-h-screen">
+        <div className="flex justify-between bg-teal-500 mb-8 px-6 py-4 text-teal-100">
+          <h1 className="font-bold text-lg">MyWebsite</h1>
+          <nav className="flex gap-4">
+            <a href="#">Home</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact</a>
+          </nav>
+        </div>
+        <div>
+          <section className="text-center">
+            <h2 className="mb-4 font-semibold sm:text-amber-500 text-xl">
+              Welcome
+            </h2>
+            <p>This is the main content area of the website.</p>
+          </section>
+          <section>
+            <h2 className="mb-4 font-semibold text-xl text-center">Features</h2>
+            <div className="gap-6 grid sm:grid-cols-2 md:grid-cols-3">
+              <div className="bg-teal-400 p-4">Feature 1</div>
+              <div className="bg-teal-400 p-4">Feature 2</div>
+              <div className="bg-teal-400 p-4">Feature 3</div>
+              <div className="bg-teal-400 p-4">Feature 1</div>
+              <div className="bg-teal-400 p-4">Feature 2</div>
+              <div className="bg-teal-400 p-4">Feature 3</div>
+              <div className="bg-teal-400 p-4">Feature 1</div>
+              <div className="bg-teal-400 p-4">Feature 2</div>
+              <div className="bg-teal-400 p-4">Feature 3</div>
+            </div>
+          </section>
+        </div>
+        <div> Footer </div>
+      </div>
+    </>
+  );
+}
 
-export default App
+export default App;
